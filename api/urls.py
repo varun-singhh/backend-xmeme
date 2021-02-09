e01952b76d29b7a14470eb9aa90efbdccba5a7f6
Varun Singh <47475007+varun-singhh@users.noreply.github.com>
2021-02-09 10:21:50 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.apiOverview,name="api-overview"),
     path('memes/',views.taskList,name="memes-list"),
-    path('memes-detail/<str:pk>/',views.taskDetail,name="memes-detail"),
+    path('memes/<str:pk>/',views.taskDetail,name="memes-detail"),
     path('memes-create/',views.taskCreate,name="memes-create"),
     path('memes-update/<str:pk>/',views.taskUpdate,name="memes-update"),
     path('memes-delete/<str:pk>/',views.taskDelete,name="memes-delete"),
